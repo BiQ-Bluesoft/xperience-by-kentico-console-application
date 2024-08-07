4373a6462854982b77110418aeeaf65ab1d18fd5
Milan Šůstek <milan.sustek@bluesoft.cz>
2024-08-07 16:34:30 +0200

    Console application working

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CMS.Core;
+using CMS.DataEngine;
+using Microsoft.Extensions.Configuration;
+
+Service.Use<IConfiguration>(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build());
+
+CMSApplication.Init();
